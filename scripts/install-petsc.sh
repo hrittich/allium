c61dcef0d7b4f5b0368aeab3142e13a7c4bb3191
Hannah Rittich <hrittich@users.noreply.github.com>
2021-06-10 11:47:56 +0200

    Updated PETSc in the container images.

--- scripts/install-petsc.sh
+++ scripts/install-petsc.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 PETSC_URL=""
+VERSION=3.15.0
+CHECKSUM=ac46db6bfcaaec8cd28335231076815bd5438f401a4a05e33736b4f9ff12e59a
 
 function download {
-  (wget --progress=dot:mega --tries=3 http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.13.3.tar.gz ||
-   wget --progress=dot:mega https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-lite-3.13.3.tar.gz) &&
-  echo "dc744895ee6b9c4491ff817bef0d3abd680c5e3c25e601be44240ce65ab4f337  petsc-lite-3.13.3.tar.gz" | sha256sum -c
+  (wget --progress=dot:mega --tries=3 https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-${VERSION}.tar.gz ||
+   wget --progress=dot:mega https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-${VERSION}.tar.gz) &&
+  echo "${CHECKSUM}  petsc-${VERSION}.tar.gz" | sha256sum -c
 }
 
 function install {
-  tar -axf petsc-lite-3.13.3.tar.gz && \
-  (cd petsc-3.13.3 && \
+  tar -axf petsc-${VERSION}.tar.gz && \
+  (cd petsc-${VERSION} && \
    ./configure --prefix=/usr/local --with-scalar-type=complex --with-fortran-kernels=1 && \
    make && \
    sudo make install) && \
-  rm -r petsc-3.13.3
+  rm -r petsc-${VERSION}
 }
 
 CMD="${1:-all}"
