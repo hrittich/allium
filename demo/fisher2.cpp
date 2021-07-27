c91a85650cfc3e442da4fae2d0229f74f1ce6f9b
Hannah Rittich <hrittich@users.noreply.github.com>
2021-07-27 16:58:31 +0200

    Refactored heat equation and Fisher's equation demos.

--- demo/fisher2.cpp
+++ demo/fisher2.cpp
@@ -31,11 +31,113 @@ using Mesh = PetscMesh<double, 2>;
 using LocalMesh = PetscLocalMesh<double, 2>;
 
 /** Stores the problem specific parameters. */
-struct Problem {
-  double alpha;
-  double beta;
-  double h;
+class Fisher {
+  public:
+    Fisher(int N, double alpha, double beta);
+
+    void simulate();
+
+  private:
+    const global_size_t m_N;
+    const double m_h;
+    const double m_alpha;
+    const double m_beta;
+
+    double exact_solution(double t, double x, double y);
+
+    void zero_boundary(::LocalMesh& mesh);
+
+    void add_boundary(Mesh& mesh, double t);
+
+    void set_solution(Mesh& result, double t);
+
+    void apply_shifted_laplace(Mesh& f, Number a, const Mesh& u);
+
+    void solve_f_impl(Mesh& y,
+                      Real t,
+                      Number a,
+                      const Mesh& r,
+                      InitialGuess initial_guess);
+
+    static void f_expl(Mesh& result, Real t, const Mesh& u);
 };
+
+int main(int argc, char** argv)
+{
+  Init init(argc, argv);  // initialize Allium
+
+  Fisher problem(64, 1.0, 0.5);
+  problem.simulate();
+
+  return 0;
+}
+
+Fisher::Fisher(int N, double alpha, double beta)
+  : m_N(N),
+    m_h(20.0 / (N-1)),
+    m_alpha(alpha),
+    m_beta(beta)
+{}
+
+void Fisher::simulate() {
+  using namespace std::placeholders;
+
+  auto comm = Comm::world();
+
+  if (comm.rank() == 0)
+    std::cout << "Fisher 2D solver" << std::endl;
+
+  // Create a mesh. For description of the parameters, see the PETSc manual
+  auto spec = std::shared_ptr<PetscMeshSpec<2>>(
+                new PetscMeshSpec<2>(
+                  comm,
+                  {DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED},
+                  DMDA_STENCIL_STAR,
+                  {m_N, m_N}, // global size
+                  {PETSC_DECIDE, PETSC_DECIDE}, // processors per dim
+                  1, // ndof
+                  1)); // stencil_width
+
+  Mesh u(spec);
+  Mesh error(spec);
+
+  // setup the integrator
+  ImexEuler<Mesh> integrator;
+  integrator.setup(f_expl,
+                   [](Mesh& out, double t, const Mesh& in) {}, // not needed for Euler
+                   std::bind(&Fisher::solve_f_impl, this, _1, _2, _3, _4, _5));
+
+  double t0 = 0;
+  set_solution(u, t0);
+  integrator.initial_values(t0, u);
+  integrator.dt(0.01);
+
+  auto filename = [](int frame) {
+    std::stringstream s;
+    s << "mesh_" << frame << ".pvti";
+    return s.str();
+  };
+
+  write_vtk(filename(0), u);
+
+  for (int i = 0; i < 200; ++i) {
+    double t0 = i * 0.1;
+    double t1 = (i+1)*0.1;
+    integrator.initial_values(t0, u);
+    integrator.integrate(u, t1);
+
+    write_vtk(filename(i+1), u);
+
+    // error = exact - u
+    set_solution(error, t1);
+    error.add_scaled(-1.0, u);
+
+    auto e = error.l2_norm();
+    if (comm.rank() == 0) {
+      std::cout << "t = " << t1 << ", ‖e‖ = " << e << std::endl;
+    }
+  }
+}
 
 /**
   The exact analytic solution of the problem.
@@ -53,11 +155,11 @@ struct Problem {
   American Journal of Physics, American Journal of Physics, 60 (7): 650–54.
   https://doi.org/10.1119/1.17120.
 */
-double exact_solution(Problem pb, double t, double x, double y)
+double Fisher::exact_solution(double t, double x, double y)
 {
-  double norm = sqrt(pb.alpha*pb.alpha + pb.beta*pb.beta);
-  double alpha = pb.alpha / norm;
-  double beta = pb.beta / norm;
+  double norm = sqrt(m_alpha*m_alpha + m_beta*m_beta);
+  double alpha = m_alpha / norm;
+  double beta = m_beta / norm;
 
   double r = alpha * x + beta * y;
 
@@ -68,7 +170,7 @@ double exact_solution(Problem pb, double t, double x, double y)
 /**
   Set the boundary values of the mesh to zero.
 */
-void zero_boundary(::LocalMesh& mesh)
+void Fisher::zero_boundary(::LocalMesh& mesh)
 {
   // the range of the whole mesh
   auto global_range = mesh.mesh_spec()->range();
@@ -94,7 +196,7 @@ void zero_boundary(::LocalMesh& mesh)
 
 /** Add the contribution of the boundary points when applying the Laplace
  operator to the given vector. */
-void add_boundary(Mesh& mesh, Problem pb, double t)
+void Fisher::add_boundary(Mesh& mesh, double t)
 {
   // the range of the whole mesh
   auto global_range = mesh.mesh_spec()->range();
@@ -107,50 +209,50 @@ void add_boundary(Mesh& mesh, Problem pb, double t)
   // be transferred from an accelerator.
   auto lmesh = local_mesh(mesh);
 
-  double h = pb.h;
+  const auto h = m_h;
 
   // Iterate over all mesh points
   for (auto p : range) {
     if (p[0] == 0) { // left boundary
       double x = -1.0 * h;
       double y = p[1] * h;
-      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(pb, t, x, y);
+      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(t, x, y);
     }
     if (p[1] == 0) { // top boundary
       double x = p[0] * h;
       double y = -1 * h;
-      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(pb, t, x, y);
+      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(t, x, y);
     }
     if (p[0] == global_range.end_pos()[0]-1) { // right boundary
       double x = global_range.end_pos()[0] * h;
       double y = p[1] * h;
-      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(pb, t, x, y);
+      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(t, x, y);
     }
     if (p[1] == global_range.end_pos()[1]-1) { // right boundary
       double x = p[0] * h;
       double y = global_range.end_pos()[1] * h;
-      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(pb, t, x, y);
+      lmesh(p[0], p[1]) += (1.0 / (h*h)) * exact_solution(t, x, y);
     }
   }
 }
 
 /** Set the mesh values to the exact solution. */
-void set_solution(Mesh& result, Problem pb, double t)
+void Fisher::set_solution(Mesh& result, double t)
 {
   auto range = result.mesh_spec()->local_range();
 
   auto lresult = local_mesh(result);
   for (auto p : range) {
-    double x = pb.h * p[0];
-    double y = pb.h * p[1];
-    lresult(p[0], p[1]) = exact_solution(pb, t, x, y);
+    double x = m_h * p[0];
+    double y = m_h * p[1];
+    lresult(p[0], p[1]) = exact_solution(t, x, y);
   }
 }
 
 /**
  Compute `f = (-Δ + a I) u`.
  */
-void apply_shifted_laplace(Mesh& f, Problem pb, Number a, const Mesh& u)
+void Fisher::apply_shifted_laplace(Mesh& f, Number a, const Mesh& u)
 {
   // PETSc require to create a "local mesh" to have access to the ghost
   // nodes.
@@ -171,8 +273,8 @@ void apply_shifted_laplace(Mesh& f, Problem pb, Number a, const Mesh& u)
   //         |     -1     |h
   for (auto p : range) {
     lf(p[0], p[1])
-      = (1.0 / (pb.h*pb.h))
-        * ( (4+a*(pb.h*pb.h)) * lu(p[0],   p[1])
+      = (1.0 / (m_h*m_h))
+        * ( (4+a*(m_h*m_h)) * lu(p[0],   p[1])
             - lu(p[0]-1, p[1])
             - lu(p[0],   p[1]-1)
             - lu(p[0],   p[1]+1)
@@ -183,31 +285,28 @@ void apply_shifted_laplace(Mesh& f, Problem pb, Number a, const Mesh& u)
 /**
  Solves y - a f_i(t, y) = r, where f_i(t, y) = Δy.
 */
-void solve_f_impl(Mesh& y,
-                  Problem pb,
-                  Real t,
-                  Number a,
-                  const Mesh& r,
-                  InitialGuess initial_guess) {
+void Fisher::solve_f_impl(Mesh& y,
+                          Real t,
+                          Number a,
+                          const Mesh& r,
+                          InitialGuess initial_guess) {
   using namespace std::placeholders;
 
   // rhs = (1/a) r + Δ^b u^b
   Mesh rhs(r.mesh_spec());
   rhs.assign(r);
   rhs *= (1.0/a);
-  add_boundary(rhs, pb, t);
+  add_boundary(rhs, t);
 
   // solve (-Δ + (1/a) I) y = (1/a) r + Δ^b y^b
   CgSolver<Mesh> solver;
-  auto op = std::bind(apply_shifted_laplace, _1, pb, 1.0/a, _2);
+  auto op = std::bind(&Fisher::apply_shifted_laplace, this, _1, 1.0/a, _2);
   solver.setup(shared_copy(make_linear_operator<Mesh>(op)));
   solver.solve(y, rhs, initial_guess);
-
-  std::cout << "#iterations = " << solver.iteration_count() << std::endl;
 };
 
 /** The explicit part of the ODE, f_e(y) = y*(1-y) */
-void f_expl(Mesh& result, Real t, const Mesh& u)
+void Fisher::f_expl(Mesh& result, Real t, const Mesh& u)
 {
   auto range = u.mesh_spec()->local_range();
   auto lresult = local_mesh(result);
@@ -216,75 +315,5 @@ void f_expl(Mesh& result, Real t, const Mesh& u)
   for (auto p : range) {
     lresult(p[0], p[1]) = lu(p[0], p[1]) * (1.0 - lu(p[0], p[1]));
   }
-}
-
-int main(int argc, char** argv)
-{
-  using namespace std::placeholders;
-
-  Init init(argc, argv);  // initialize Allium
-
-  const int N = 64;
-  double h = 20.0 / (N-1);
-
-  auto comm = Comm::world();
-
-  if (comm.rank() == 0)
-    std::cout << "Fisher 2D solver" << std::endl;
-
-  // Create a mesh. For description of the parameters, see the PETSc manual
-  auto spec = std::shared_ptr<PetscMeshSpec<2>>(
-                new PetscMeshSpec<2>(
-                  comm,
-                  {DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED},
-                  DMDA_STENCIL_STAR,
-                  {N, N}, // global size
-                  {PETSC_DECIDE, PETSC_DECIDE}, // processors per dim
-                  1, // ndof
-                  1)); // stencil_width
-
-  Mesh u(spec);
-  Mesh error(spec);
-
-  Problem pb = { 1.0, 0.5, h };
-
-  // setup the integrator
-  ImexEuler<Mesh> integrator;
-  integrator.setup(f_expl,
-                   [](Mesh& out, double t, const Mesh& in) {}, // not needed for Euler
-                   std::bind(solve_f_impl, _1, pb, _2, _3, _4, _5));
-
-  double t0 = 0;
-  set_solution(u, pb, t0);
-  integrator.initial_values(t0, u);
-  integrator.dt(0.01);
-
-  auto filename = [](int frame) {
-    std::stringstream s;
-    s << "mesh_" << frame << ".pvti";
-    return s.str();
-  };
-
-  write_vtk(filename(0), u);
-
-  for (int i = 0; i < 200; ++i) {
-    double t0 = i * 0.1;
-    double t1 = (i+1)*0.1;
-    integrator.initial_values(t0, u);
-    integrator.integrate(u, t1);
-
-    write_vtk(filename(i+1), u);
-
-    // error = exact - u
-    set_solution(error, pb, t1);
-    error.add_scaled(-1.0, u);
-
-    auto e = error.l2_norm();
-    if (comm.rank() == 0) {
-      std::cout << "t = " << t1 << ", ‖e‖ = " << e << std::endl;
-    }
-  }
-
-  return 0;
 }
 
--- demo/heat_periodic2.cpp
+++ demo/heat_periodic2.cpp
@@ -30,98 +30,32 @@ using Real = real_part_t<Number>;
 using Mesh = PetscMesh<double, 2>;
 using LocalMesh = PetscLocalMesh<double, 2>;
 
-constexpr double pi = 4.0 * atan(1.0);
+const double pi = 4.0 * atan(1.0);
 
-/** Stores the problem specific parameters. */
-struct Problem {
-  double h;
+/** Simulation of the heat equation. */
+class Heat {
+  public:
+    Heat(int N);
+
+    void simulate();
+
+  private:
+    const global_size_t m_N;
+    const double m_h;
+
+    static double exact_solution(double t, double x, double y);
+    void set_solution(Mesh& result, double t);
+
+    void apply_shifted_laplace(Mesh& f, Number a, const Mesh& u);
+
+    void solve_f_impl(Mesh& y,
+                      Real t,
+                      Number a,
+                      const Mesh& r,
+                      InitialGuess initial_guess);
+
+    static void f_expl(Mesh& result, Real t, const Mesh& u);
 };
-
-/**
-  The exact analytic solution of the problem.
-  The solution for this particular case is known, hence, we can check the
-  correctness of our code.
-*/
-double exact_solution(Problem pb, double t, double x, double y)
-{
-  return exp(-8*pi*pi*t) * sin(x*pi*2) * sin(y*pi*2);
-}
-
-/** Set the mesh values to the exact solution. */
-void set_solution(Mesh& result, Problem pb, double t)
-{
-  auto range = result.mesh_spec()->local_range();
-
-  auto lresult = local_mesh(result);
-  for (auto p : range) {
-    double x = pb.h * p[0];
-    double y = pb.h * p[1];
-    lresult(p[0], p[1]) = exact_solution(pb, t, x, y);
-  }
-}
-
-/**
- Compute `f = (-Δ + a I) u`.
- */
-void apply_shifted_laplace(Mesh& f, Problem pb, Number a, const Mesh& u)
-{
-  // PETSc require to create a "local mesh" to have access to the ghost
-  // nodes.
-  ::LocalMesh u_aux(u.mesh_spec());
-  u_aux.assign(u); // copy to determine the ghost nodes
-
-  auto range = u.mesh_spec()->local_range();
-  auto lu = local_mesh(u_aux);
-  auto lf = local_mesh(f);
-
-  // Apply the stencil
-  //         |     -1     |
-  // (1/h^2) | -1   4  -1 |
-  //         |     -1     |h
-  for (auto p : range) {
-    lf(p[0], p[1])
-      = (1.0 / (pb.h*pb.h))
-        * ( (4+a*(pb.h*pb.h)) * lu(p[0],   p[1])
-            - lu(p[0]-1, p[1])
-            - lu(p[0],   p[1]-1)
-            - lu(p[0],   p[1]+1)
-            - lu(p[0]+1, p[1]));
-  }
-}
-
-/**
- Solves y - a f_i(t, y) = r, where f_i(t, y) = Δy.
-*/
-void solve_f_impl(Mesh& y,
-                  Problem pb,
-                  Real t,
-                  Number a,
-                  const Mesh& r,
-                  InitialGuess initial_guess) {
-  using namespace std::placeholders;
-
-  // rhs = (1/a) r + Δ^b u^b
-  Mesh rhs(r.mesh_spec());
-  rhs.assign(r);
-  rhs *= (1.0/a);
-
-  // solve (-Δ + (1/a) I) y = (1/a) r + Δ^b y^b
-  CgSolver<Mesh> solver;
-  auto op = std::bind(apply_shifted_laplace, _1, pb, 1.0/a, _2);
-  solver.setup(shared_copy(make_linear_operator<Mesh>(op)));
-  solver.solve(y, rhs, initial_guess);
-}
-
-/** The explicit part of the ODE, f_e(y) = 0 */
-void f_expl(Mesh& result, Real t, const Mesh& u)
-{
-  auto range = u.mesh_spec()->local_range();
-  auto lresult = local_mesh(result);
-
-  for (auto p : range) {
-    lresult(p[0], p[1]) = 0.0;
-  }
-}
 
 int main(int argc, char** argv)
 {
@@ -129,8 +63,19 @@ int main(int argc, char** argv)
 
   Init init(argc, argv);  // initialize Allium
 
-  const int N = 64;
-  double h = 1.0 / N; // in the periodic case h is 1.0 / N
+  Heat problem(64);
+  problem.simulate();
+
+  return 0;
+}
+
+Heat::Heat(int N)
+  : m_N(N),
+    m_h(1.0 / N) // in the periodic case h is 1.0 / N
+{}
+
+void Heat::simulate() {
+  using namespace std::placeholders;
 
   auto comm = Comm::world();
 
@@ -143,7 +88,7 @@ int main(int argc, char** argv)
                   comm,
                   {DM_BOUNDARY_PERIODIC, DM_BOUNDARY_PERIODIC},
                   DMDA_STENCIL_STAR,
-                  {N, N}, // global size
+                  {m_N, m_N}, // global size
                   {PETSC_DECIDE, PETSC_DECIDE}, // processors per dim
                   1, // ndof
                   1)); // stencil_width
@@ -151,16 +96,14 @@ int main(int argc, char** argv)
   Mesh u(spec);
   Mesh error(spec);
 
-  Problem pb = { h };
-
   // setup the integrator
   ImexEuler<Mesh> integrator;
-  integrator.setup(f_expl,
+  integrator.setup(&f_expl,
                    [](Mesh& out, double t, const Mesh& in) {}, // not needed for Euler
-                   std::bind(solve_f_impl, _1, pb, _2, _3, _4, _5));
+                   std::bind(&Heat::solve_f_impl, this, _1, _2, _3, _4, _5));
 
   double t0 = 0;
-  set_solution(u, pb, t0);
+  set_solution(u, t0);
   integrator.initial_values(t0, u);
   integrator.dt(1e-4);
 
@@ -181,15 +124,98 @@ int main(int argc, char** argv)
     write_vtk(filename(i+1), u);
 
     // error = exact - u
-    set_solution(error, pb, t1);
+    set_solution(error, t1);
     error.add_scaled(-1.0, u);
 
     auto e = error.l2_norm();
     if (comm.rank() == 0) {
       std::cout << "t = " << t1 << ", ‖e‖ = " << e << std::endl;
     }
-  }
+  } 
+}
 
-  return 0;
+/**
+  The exact analytic solution of the problem.
+  The solution for this particular case is known, hence, we can check the
+  correctness of our code.
+*/
+double Heat::exact_solution(double t, double x, double y)
+{
+  return exp(-8*pi*pi*t) * sin(x*pi*2) * sin(y*pi*2);
+}
+
+/** Set the mesh values to the exact solution. */
+void Heat::set_solution(Mesh& result, double t)
+{
+  auto range = result.mesh_spec()->local_range();
+
+  auto lresult = local_mesh(result);
+  for (auto p : range) {
+    double x = m_h * p[0];
+    double y = m_h * p[1];
+    lresult(p[0], p[1]) = exact_solution(t, x, y);
+  }
+}
+
+/**
+ Compute `f = (-Δ + a I) u`.
+ */
+void Heat::apply_shifted_laplace(Mesh& f, Number a, const Mesh& u)
+{
+  // PETSc require to create a "local mesh" to have access to the ghost
+  // nodes.
+  ::LocalMesh u_aux(u.mesh_spec());
+  u_aux.assign(u); // copy to determine the ghost nodes
+
+  auto range = u.mesh_spec()->local_range();
+  auto lu = local_mesh(u_aux);
+  auto lf = local_mesh(f);
+
+  // Apply the stencil
+  //         |     -1     |
+  // (1/h^2) | -1   4  -1 |
+  //         |     -1     |h
+  for (auto p : range) {
+    lf(p[0], p[1])
+      = (1.0 / (m_h*m_h))
+        * ( (4+a*(m_h*m_h)) * lu(p[0], p[1])
+            - lu(p[0]-1, p[1])
+            - lu(p[0],   p[1]-1)
+            - lu(p[0],   p[1]+1)
+            - lu(p[0]+1, p[1]));
+  }
+}
+
+/**
+ Solves y - a f_i(t, y) = r, where f_i(t, y) = Δy.
+*/
+void Heat::solve_f_impl(Mesh& y,
+                        Real t,
+                        Number a,
+                        const Mesh& r,
+                        InitialGuess initial_guess) {
+  using namespace std::placeholders;
+
+  // rhs = (1/a) r + Δ^b u^b
+  Mesh rhs(r.mesh_spec());
+  rhs.assign(r);
+  rhs *= (1.0/a);
+
+  // solve (-Δ + (1/a) I) y = (1/a) r + Δ^b y^b
+  CgSolver<Mesh> solver;
+  auto op = std::bind(&Heat::apply_shifted_laplace, this, _1, 1.0/a, _2);
+  solver.setup(shared_copy(make_linear_operator<Mesh>(op)));
+  solver.solve(y, rhs, initial_guess);
+}
+
+/** The explicit part of the ODE, f_e(y) = 0 */
+void Heat::f_expl(Mesh& result, Real t, const Mesh& u)
+{
+  auto range = u.mesh_spec()->local_range();
+  auto lresult = local_mesh(result);
+
+  for (auto p : range) {
+    lresult(p[0], p[1]) = 0.0;
+  }
 }
 
