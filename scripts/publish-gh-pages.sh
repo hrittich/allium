8018c08632e9a848588ccb74f0d9e27fb0ee024f
Hannah Rittich <hrittich@users.noreply.github.com>
2022-02-08 17:11:02 +0100

    WIP

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -xe
 
-git switch -c gh-pages
-
 cmake -S . -B build
 make -C build api-docs
 
@@ -14,8 +12,9 @@ fi
 
 git add --force build/doc/
 git commit -m 'Publish Pages'
+git subtree split --branch=gh-pages --prefix=build/doc/
 #git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-#git push --force --set-upstream origin gh-pages
-git subtree push --force --prefix gh-pages origin gh-pages
+git push --force origin gh-pages
+#git subtree push --force --prefix gh-pages origin gh-pages
 
 
