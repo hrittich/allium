a9abc621c1a7475b50b2326938e8cad072c4bc13
Hannah Rittich <hrittich@users.noreply.github.com>
2022-02-08 13:55:26 +0100

    WIP

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -10,6 +10,8 @@ TMPDIR="$(mktemp -d --tmpdir gh-pagesXXXXXXXX)"
 cp -r doc/html/ -T "$TMPDIR"
 
 cd "$TMPDIR"
+git config --global user.email "publish@bots.github.com"
+git config --global user.name "Publish Bot"
 git init .
 git switch -c gh-pages
 git add .
